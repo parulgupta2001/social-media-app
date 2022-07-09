99bebd7a05ee0d96aea9a484f429e8629cff2c32
Parul Gupta <94689580+parulgupta2001@users.noreply.github.com>
2022-07-09 10:59:31 +0530

    feat- authentication using redux-toolkit

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -3,15 +3,16 @@ import { formatDate } from "../utils/authUtils";
 /**
  * User Database can be added here.
  * You can add default users of your wish with different attributes
+ * Every user will have likes (Likes are set to 0 by default), History Array, Playlists Array (added Watch Later Playlist in it by default) by default
  * */
 
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    username: "adarshbalika",
-    password: "adarshBalika123",
+    firstName: "Parul",
+    lastName: "Gupta",
+    username: "parulgupta",
+    password: "parul123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
